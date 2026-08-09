13f4f1878a4d4096c6824ca9d16d3cf04cff7043
cpf <the.spiderminecart.0@gmail.com>
2026-08-09 00:02:02 -0700

    layout writer now actualy works, some trash with layout loader, next commit will be cleaner

--- world_layout/layout_writer.gd
+++ world_layout/layout_writer.gd
@@ -1,11 +1,102 @@
 extends TileMapLayer
 
-@export var start : Vector2i
+@export var file_path : String = "res://assets/world_layout.lyt.txt"
+@export var start_room_idx : Vector2i
 @export var dbg : bool = !false
 
-func _ready():
-	var k = [[0]]
+func _ready() -> void :
+	var repr : Array = [[0]]
+	var start : Vector2i = Vector2i.ZERO
+	var width: int = 1
+	var height: int = 1
 	
+	for idx : Vector2i in get_used_cells() :
+		while idx.x < start.x :
+			start.x -= 1
+			width += 1
+			#for row in repr : row.push_front(inc)
+			for row in repr : row.push_front(0)
+			#for row_idx in repr.size() : repr[row_idx].push_front(Vector2i(start.x,row_idx-start.y))
+			print("push left")
+		
+		while idx.x >= width+start.x :
+			width += 1
+			#for row in repr : row.push_back(inc)
+			for row in repr : row.push_back(0)
+			#for row_idx in repr.size() : repr[row_idx].push_back(Vector2i(width+start.x,row_idx-start.y))
+			print("push right")
+		
+		while idx.y < start.y :
+			start.y -= 1
+			height += 1
+			var array : Array = []
+			for unit in range(0, width) : 
+				#array.push_back(inc)
+				array.push_back(0)
+				#array.push_back(Vector2i(unit+start.x,start.y+height))
+			repr.push_front(array)
+			print("push top")
+		
+		while idx.y >= height+start.y :
+			height += 1
+			var array : Array = []
+			for unit in range(0, width) : 
+				#array.push_back(inc)
+				array.push_back(0)
+				#array.push_back(Vector2i(unit+start.x,start.y+height))
+			repr.push_back(array)
+			print("push bottom")
+		
+		#repr[idx.y-start.y][idx.x-start.x] = id_from_coords(get_cell_atlas_coords(idx))
+		#repr[-start.y][-start.x] = 1
+		
+		#if (
+			#(idx.y-start.y >= 0) && 
+			#(idx.y-start.y < repr.size()) && 
+			#(idx.x-start.x >= 0) && 
+			#(idx.x-start.x < repr[idx.y-start.y].size())
+		#):
+		repr[idx.y-start.y][idx.x-start.x] = id_from_coords(get_cell_atlas_coords(idx))
+		
+		print(str(idx))
+		print("start"+str(start))
+		print("dim("+str(width)+", "+str(height)+")")
+		for row in repr :
+			var tem = ""
+			for chr in row :
+				tem+= "_" if chr == 0 else "@"
+				#tem+=str(chr)+", "
+			print(tem)
+		print("\n")
+	
+	file_write(repr, start)
+
+func file_write(repr: Array, start: Vector2i) -> void :
+	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	file.store_string(
+		"s"+str(start_room_idx.x)+":"+str(start_room_idx.y)+
+		", o"+str(start_room_idx.x-start.x-1)+":"+str(start_room_idx.y-start.y-1)+
+		", \n"
+	)
+	for rep in repr :
+		var temp : String = ""
+		var count : int = 0
+		for to_proc_idx : int in range(0, rep.size()) :
+			var last : bool = to_proc_idx == rep.size()-1
+			var to_proc : int = rep[to_proc_idx]
+			var next_proc : int = rep[to_proc_idx+(0 if last else 1)]
+			
+			count+=1
+			if last or to_proc != next_proc :
+				if count > 1 :
+					temp+="'"+str(count)+":"+str(to_proc)+", "
+				else : temp+=str(to_proc)+", "
+				count = 0
+		if dbg : print(temp)
+		file.store_string(temp+"\n")
+	file.close()
+
+func kready():
 	# start_dim is the width and height in the ..0 direction
 	var start_width: int = 0
 	var start_height: int = 0
@@ -54,8 +145,8 @@ func _ready():
 	
 	var file = FileAccess.open("res://assets/world_layout.lyt.txt", FileAccess.WRITE)
 	file.store_string(
-		"s"+str(start.x)+":"+str(start.y)+
-		", o"+str(start.x-start_width-1)+":"+str(start.y-start_height-1)+
+		"s"+str(start_room_idx.x)+":"+str(start_room_idx.y)+
+		", o"+str(start_room_idx.x-start_width-1)+":"+str(start_room_idx.y-start_height-1)+
 		", w"+str(abs(start_width)+abs(end_width)+1)+
 		", t"+str(start_height)+
 		", l"+str(start_width)+
