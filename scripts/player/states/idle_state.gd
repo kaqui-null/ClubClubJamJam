07416c79f5b44d12334ad3ca935e1d64ec4f4c73
Rennan555 <dungeonbr5@gmail.com>
2026-08-22 14:17:25 -0300

    feat: Initial Walk and Idle States added to Player

---
+++ scripts/player/states/idle_state.gd
@@ -0,0 +1,20 @@
+class_name IdleState
+extends State
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func enter() -> void:
+	pass
+
+func update(delta: float) -> void:
+	pass
+
+func physics_update(delta: float) -> void:
+	pass
+
+func exit() -> void:
+	pass
