6b70b291757e3ecedc2571d42f0970fc54ca7641
Adject <adjective.blazkowicz@gmail.com>
2026-08-29 13:53:49 +0200

    remove the room properties resource as it was unnecessary for the specific purpose it was used for here

--- world_layout/room.gd
+++ world_layout/room.gd
@@ -1,31 +1,61 @@
-@tool
 class_name Room
 extends Node2D
 
-@export var room_properties: RoomProperties:
-	set(new_properties):
-		if Engine.is_editor_hint():
-			update = new_properties.update_exits
-			new_properties.Room = self
-		room_properties = new_properties
-@export_tool_button("Update Exits", "Reload") var update;
-
-func connect_to_room(target: Room, opposing_dir: RoomProperties.ExitDir) -> void:
-	var vec_mapped_to_dir := [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT]
-	var tilesize: int = $Exits.tile_set.tile_size.x
-	var target_properties := target.room_properties 
-	var dir: RoomProperties.ExitDir = target_properties.opposite(opposing_dir)
-	var placement_location: Vector2 = target.global_position
-	
-	assert(target_properties.exit_exists[opposing_dir], 
-	"Attempted to place room at nonexistent exit.")
-	assert(room_properties.exit_exists[dir],
-	"No compatible exit to connect to the room.")
-	placement_location += tilesize * Vector2(target_properties.exits[opposing_dir])
-	placement_location += tilesize * vec_mapped_to_dir[opposing_dir]
-	placement_location -= tilesize * Vector2(room_properties.exits[dir])
-	global_position = placement_location
+var exits: Array[Vector2i];
+var exit_exists: Array[bool]; 
+enum ExitDir {UP, RIGHT, DOWN, LEFT} # clockwise
 
 func _ready() -> void:
 	if not Engine.is_editor_hint():
+		update_exits()
 		$Exits.visible = false
+
+func connect_to_room(target: Room, opposing_dir: ExitDir) -> void:
+	var vec_mapped_to_dir := [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT]
+	var tilesize: int = $Exits.tile_set.tile_size.x 
+	var dir: ExitDir = opposite(opposing_dir)
+	var placement_location: Vector2 = target.global_position
+	
+	assert(target.exit_exists[opposing_dir], 
+	"Attempted to place room at nonexistent exit.")
+	assert(exit_exists[dir],
+	"No compatible exit to connect to the room.")
+	placement_location += tilesize * (Vector2(target.exits[opposing_dir])
+						+ vec_mapped_to_dir[opposing_dir] 
+						- Vector2(exits[dir]))
+	global_position = placement_location
+		
+func update_exits() -> void:
+	var cells: Array[Vector2i];
+
+	for dir in ExitDir.values():
+		cells = $Exits.get_used_cells_by_id(-1, Vector2i(int(dir), 0))
+		if cells.size() == 1:
+			exit_exists[dir] = true
+			# i cannot just assign it without using Vector2i() constructor, idk why
+			exits[dir] = Vector2i(cells[0])
+		elif cells.size() == 0:
+			exit_exists[dir] = false
+		else:
+			push_error("Two or more exits of the " + dir_name(dir) + " direction, only one instance is allowed.")
+			return
+	print_exits()
+
+func print_exits() -> void:
+	print(" ")
+	for dir in ExitDir.values():
+		if exit_exists[dir]:
+			print(dir_name(dir) + " = " + str(exits[dir]))
+		else:
+			print(dir_name(dir) + " = " + "Absent")
+	print(" ")
+
+func dir_name(dir: ExitDir) -> String:
+	return ExitDir.keys()[dir]
+
+func opposite(dir: ExitDir) -> ExitDir:
+	if dir > 1:
+		dir -= 2
+	else:
+		dir += 2
+	return dir
